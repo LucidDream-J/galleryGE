20f140a84a0a9bea78f2fd751673b6d4bce5309a
Lucid Dreamer <48848408+LucidDream-J@users.noreply.github.com>
2021-07-19 22:49:07 +0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,79 @@
+const open_btn = document.querySelector(".open-btn");
+const close_btn = document.querySelector(".close-btn");
+const nav = document.querySelectorAll(".nav");
+const floatingBTN = document.querySelector(".footerBtn");
+
+// attach an event listener
+floatingBTN.addEventListener("click", (e) => {
+  // select the footer
+  const footer = document.querySelector(".footer");
+
+  // check if the class exists (active) then remove
+  if (footer.classList.contains("active")) {
+    // remove the class
+    footer.classList.remove("active");
+    // return the original text
+    e.target.innerText = "Terms, Privacy, Currency & more";
+  } else {
+    // we add the active class to the footer
+    footer.classList.add("active");
+
+    // add the close text
+    e.target.innerText = "X Close";
+  }
+});
+
+open_btn.addEventListener("click", () => {
+  nav.forEach((nav_el) => nav_el.classList.add("visible"));
+});
+
+close_btn.addEventListener("click", () => {
+  nav.forEach((nav_el) => nav_el.classList.remove("visible"));
+});
+
+// Hearts//////////////////////////////////////////////////
+
+// const loveMe = document.querySelector(".loveMe");
+// const times = document.querySelector("#times");
+// let clickTime = 0;
+// let timesClicked = 0;
+
+// loveMe.addEventListener("click", (e) => {
+//   if (clickTime === 0) {
+//     clickTime = new Date().getTime();
+//   } else {
+//     if (new Date().getTime() - clickTime < 800) {
+//       createHeart(e);
+//       clickTime = 0;
+//     } else {
+//       clickTime = new Date().getTime();
+//     }
+//   }
+// });
+
+// function createHeart(e) {
+//   const heart = document.createElement("i");
+//   heart.classList.add("fas");
+//   heart.classList.add("fa-heart");
+
+//   const x = e.clientX;
+//   const y = e.clientY;
+
+//   const leftOffset = e.target.offsetLeft;
+//   const topOffset = e.target.offsetTop;
+
+//   const xInside = x - leftOffset;
+//   const yInside = y - topOffset;
+
+//   //   console.log(x, xInside, y, yInside);
+
+//   heart.style.top = `${yInside}px`;
+//   heart.style.left = `${xInside}px`;
+
+//   loveMe.appendChild(heart);
+//   times.textContent = ++timesClicked;
+
+//   setTimeout(() => {
+//     heart.remove();
+//   }, 1000);
+// }
