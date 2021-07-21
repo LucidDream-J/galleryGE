4999072450f7af09d999b853d58468f30b8b813d
Lucid Dreamer <48848408+LucidDream-J@users.noreply.github.com>
2021-07-21 20:41:42 +0800

    Add files via upload

--- html/artists/ContentPlaceHolders/app.js
+++ html/artists/ContentPlaceHolders/app.js
@@ -12,12 +12,11 @@ setTimeout(() => {
 }, 2000);
 
 function getData() {
-  header.innerHTML = `<img class="card-img-top" src="./images/DS3.png" alt="">`;
+  header.innerHTML = `<img class="card-img-top" src="./images/1.jpg" alt="">`;
   title.innerText = `Lorem ipsum dolor sit amet.`;
   excerpt.innerHTML = ` <p> Dr Christopher Crouch has held professorial roles in Australian and Chinese art schools and his work is held in public and private collections in Australia. This is his seventh exhibition with Gallery East.</p>
 <p>His past work has focused on parkland and urban trees, investigating their relationship to both nature and their social context. Critics have described him as ‘a consummate draughtsman’, drawing ironically on a realist tradition and making work that resembles ‘socialist realism in a slow motion car crash’.
-
- .77885.331734386911950/1238430636242316/?type=3Lenins-exile-in-Perth-1925-1953</p>
+</p>
 
 <p>His work is held in the collections of Museo Internacional de Electrografia Universidad de Castilla la Mancha, Spain; The University of Liverpool, UK, the Liverpool School of Architecture, UK, Art Bank, Wesfarmers, Western  Australia, Edith Cowan University, and Qingdao Art Gallery, the People’s Republic of China.`;
   profile_img.innerHTML = ` <img src="https://randomuser.me/api/portraits/women/45.jpg" alt=""></p>`;
