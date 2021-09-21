0d517ada80e464928c1ff85a17ad13238b3582b7
Lucid Dreamer <48848408+LucidDream-J@users.noreply.github.com>
2021-09-22 00:24:53 +0800

    Add files via upload

--- html/artists/ContentPlaceHolders/exhibAppNext.js
+++ html/artists/ContentPlaceHolders/exhibAppNext.js
@@ -14,12 +14,13 @@ setTimeout(() => {
 }, 2000);
 
 function getData() {
-  header.innerHTML = `<img id="exhibition-image" class="card-img-top" src="../images/artistsImages/nextExhib/i0TA21.png" alt="Art image">`;
+  header.innerHTML = `<img id="exhibition-image" class="card-img-top" src="../images/artistsImages/nextExhib/Atake.jpg" alt="Art image" title="Nathan Beard">`;
   title.innerText = `Five eminent  Western Australian visual artists `;
   excerpt.innerHTML = `<p>ASIAN TAKEAWAYS features the work of five eminent  Western Australian visual artists and sculptors who have  used clay as a medium for this exhibition
 </p>
 
-<p>Each artist has undertaken residencies in Asia, including  China. Their highly skilled art work undoubtedly reflects their first hand experiences  of working along side art practitioners in these countries and the practicalities of craft and art  impacting on the everyday lives of peoples both here and in those Asian countries.`;
+<p>Each artist has undertaken residencies in Asia, including  China. Their highly skilled art work undoubtedly reflects their first hand experiences  of working along side art practitioners in these countries and the practicalities of craft and art  impacting on the everyday lives of peoples both here and in those Asian countries.
+<br> <img id="exhibition-image" class="card-img-top" src="../images/artistsImages/nextExhib/i0TA21.png" alt="sponsors">`;
   profile_img.innerHTML = `<img class="profile-img" src="../images/David.jpg" alt="artist"></p>`;
   name.textContent = `David Forrest`;
   date.textContent = `September 10th, 2021`;
